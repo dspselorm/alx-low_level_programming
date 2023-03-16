34a86463982952b121e1ea3ed3be2c08b3fac437
Selorm Etse-Forfoe <selorm.etse5@gmail.com>
2023-03-16 16:33:56 -0700

    brovo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Programme Commencement Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art. \n");
+	return (0);
+}
+
