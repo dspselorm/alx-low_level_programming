b6c72d9d9131f9ee1488a4e7bcfd130ee290aec4
Selorm Etse-Forfoe <selorm.etse5@gmail.com>
2023-03-16 22:18:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
  * main - Programme Commencement Point
  * Return: Always 0 (Success)
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art. \n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
 
