88dd9c596158543e1e83516497a03a06d75494fd
Selorm Etse-Forfoe <selorm.etse5@gmail.com>
2023-03-20 14:58:01 -0700

    Print possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible different combinations
+ * of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 8; m++)
+	{
+		for (n = m + 1; n <= 9; n++)
+		{
+			putchar(m + '0');
+			putchar(n + '0');
+			if (m == 8 && n == 9)
+
+				continue;
+
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
