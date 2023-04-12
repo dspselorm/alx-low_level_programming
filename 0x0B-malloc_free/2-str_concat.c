5a724f7a2044e57c2227c5584a36642c0a41ff43
Selorm Etse-Forfoe <selorm.etse5@gmail.com>
2023-04-12 14:32:03 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,8 +20,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 	{
 		s2 = "";
-		i = ci = 0;
 	}
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -49,4 +49,3 @@ char *str_concat(char *s1, char *s2)
 	conct[i] = '\0';
 	return (conct);
 }
-
